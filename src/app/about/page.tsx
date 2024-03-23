6de3dca8c98691ecc3a9827d27299a3c5a2c02df
SonnyTaylor <santinotaylor08@gmail.com>
2024-03-23 15:34:22 +1100

    improved about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { HoverCardGithub } from "./github-hover-card";
 import { HoverCardMonkeyType } from "./monkeytype-hover-card";
 
@@ -8,36 +7,35 @@ export default function About() {
       <div className="w-2/3 min-h-screen mx-auto">
         <h1 className="py-5 text-3xl">About Me</h1>
 
-        <section>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            quae, blanditiis architecto provident aliquid ut facere, vitae
-            tenetur ipsam amet harum doloribus error voluptas sequi neque? Natus
-            explicabo vitae saepe.
+        <section className="mb-6">
+          <p className="text-base">
+            I am a passionate web developer with a strong foundation in
+            front-end technologies. I enjoy creating interactive and
+            user-friendly interfaces using HTML, CSS, and JavaScript. My primary
+            focus is on building robust web applications with React and I also
+            have experience with Python for backend development.
           </p>
         </section>
 
-        <section>
+        <section className="mb-6">
           <h2 className="pt-6 text-2xl">Skills</h2>
-          <p>
-            Im am skillful in the following areas: HTML, CSS, JavaScript, React,
-            Python and more.
+          <p className="text-base">
+            I am skillful in the following areas: HTML, CSS, JavaScript, React,
+            Python, and more. I continuously strive to expand my knowledge and
+            stay updated with the latest trends and technologies in web
+            development.
           </p>
         </section>
 
-        <section>
-          <h2 className="pt-6 text-2xl">Experience</h2>
-          <p>I have worked on severeal projects, check them out on my github</p>
-        </section>
-
-        <section>
+        <section className="mb-6">
           <h2 className="pt-6 text-2xl">Socials</h2>
-
-          <ul>
-            <li>
+          <ul className="list-none">
+            <li className="mb-2">
+              <p>Github:</p>
               <HoverCardGithub />
             </li>
-            <li>
+            <li className="mb-2">
+              <p>Monkeytype:</p>
               <HoverCardMonkeyType />
             </li>
           </ul>
