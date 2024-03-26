1d8340a276c88644e1ceba86afea85fb164561b4
SonnyTaylor <santinotaylor08@gmail.com>
2024-03-26 21:13:01 +1100

    improved about page, added blog page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,56 @@
-export default function About() {
-  return <h1>About</h1>;
+import {Footer} from "../page";
+import {HoverCardMonkeyType} from "@/app/about/monkeytype-hover-card"
+import {HoverCardGithub} from "@/app/about/github-hover-card";
+import Link from 'next/link'
+
+function Socials() {
+    return <section>
+        <h2 className="pt-5 text-2xl">Socials</h2>
+        <ul className="marker:text-white list-disc pl-4">
+            <li>
+                Github: <a target="_blank" rel="noopener noreferrer"
+                           href={"https://github.com/SonnyTaylor"}><HoverCardGithub/></a>
+            </li>
+            <li>
+                MonkeyType: <a target="_blank" rel="noopener noreferrer"
+                               href={"https://monkeytype.com/profile/salami.nipples"}><HoverCardMonkeyType/></a>
+            </li>
+        </ul>
+    </section>;
+}
+
+function AboutBio() {
+    return (
+        <p>
+            I'm a passionate and dedicated <strong>full-stack developer</strong> from{" "}
+            <strong>Melbourne, Australia</strong>. With a keen interest in both frontend and
+            backend development, I strive to create seamless and intuitive user experiences
+            while building robust and scalable applications.
+        </p>
+    )
+}
+
+function AboutNavbar() {
+    return <nav className="inline-flex space-x-3 list-none">
+        <li>
+            <Link href={"/"} className={"hover:underline"}>Home</Link>
+        </li>
+        <li>
+            <Link href={"/contact"} className={"hover:underline"}>Contact</Link>
+        </li>
+    </nav>;
+}
+
+export default function AboutPage() {
+    return (
+        <div className="dark:text-white dark:bg-black min-h-screen p-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-3xl lg:text-left pb-3">About Me</h1>
+                <AboutNavbar/>
+            </div>
+            <AboutBio/>
+            <Socials/>
+            <Footer/>
+        </div>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,38 +5,38 @@ import Link from "next/link";
 import {TextGenerateEffect} from "@/components/ui/text-generate-effect";
 
 function Navbar() {
-  const [fadeIn, setFadeIn] = useState(false);
+    const [fadeIn, setFadeIn] = useState(false);
 
-  useEffect(() => {
-    // Triggering the fade in effect
-    setFadeIn(true);
-  }, []);
+    useEffect(() => {
+        // Triggering the fade in effect
+        setFadeIn(true);
+    }, []);
 
-  return (
-    <>
-      <nav
-        className={`dark:text-white pt-5 z-20 ${
-          fadeIn ? "opacity-100 transition-opacity duration-1000" : "opacity-0"
-        }`}
-        style={{ transitionDelay: `${fadeIn ? "0.2s" : "0s"}` }}
-      >
-        <ul className="flex justify-center">
-          <li className="mr-4 hover:text-gray-400">
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li className="mr-4 hover:text-gray-400">
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav
+                className={`dark:text-white pt-5 z-20 ${
+                    fadeIn ? "opacity-100 transition-opacity duration-1000" : "opacity-0"
+                }`}
+                style={{transitionDelay: `${fadeIn ? "0.2s" : "0s"}`}}
+            >
+                <ul className="flex justify-center">
+                    <li className="mr-4 hover:text-gray-400">
+                        <Link href="/about">About Me</Link>
+                    </li>
+                    <li className="mr-4 hover:text-gray-400">
+                        <Link href="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 }
 
-function Footer() {
+export function Footer() {
     return <footer className="dark:text-white absolute bottom-0 w-full text-center p-4 text-xs">
         <p>
-            Made with{" "}
+            Made with ❤️, {" "}
             <a
                 href="https://ui.aceternity.com/"
                 target="_blank"
@@ -57,40 +57,40 @@ function Footer() {
 }
 
 export default function Home() {
-  const [particleColor, setParticleColor] = useState("#FFFFFF");
+    const [particleColor, setParticleColor] = useState("#FFFFFF");
 
-  useEffect(() => {
-    // Check if the user's system is in light mode
-    const isLightMode = window.matchMedia(
-      "(prefers-color-scheme: light)"
-    ).matches;
+    useEffect(() => {
+        // Check if the user's system is in light mode
+        const isLightMode = window.matchMedia(
+            "(prefers-color-scheme: light)"
+        ).matches;
 
-    // Set particle color based on system mode
-    setParticleColor(isLightMode ? "#000000" : "#FFFFFF");
-  }, []);
+        // Set particle color based on system mode
+        setParticleColor(isLightMode ? "#000000" : "#FFFFFF");
+    }, []);
 
-  return (
-    <div
-        className="h-[40rem] relative w-full dark:bg-black flex flex-col items-center justify-center overflow-hidden min-h-screen">
-        <div className="absolute inset-0 w-full h-screen">
-            <SparklesCore
-                id="tsparticlesfullpage"
-                background="transparent"
-                minSize={0.6}
-                maxSize={1.4}
-                particleDensity={100}
-                className="w-full h-full"
-                particleColor={particleColor}
+    return (
+        <div
+            className="h-[40rem] relative w-full dark:bg-black flex flex-col items-center justify-center overflow-hidden min-h-screen">
+            <div className="absolute inset-0 w-full h-screen">
+                <SparklesCore
+                    id="tsparticlesfullpage"
+                    background="transparent"
+                    minSize={0.6}
+                    maxSize={1.4}
+                    particleDensity={100}
+                    className="w-full h-full"
+                    particleColor={particleColor}
+                />
+            </div>
+
+            <TextGenerateEffect
+                words="Sonny Taylor"
+                className="relative z-20 text-3xl font-bold text-center text-white md:text-7xl lg:text-6xl"
             />
+            <Navbar/>
+
+            <Footer/>
         </div>
-
-        <TextGenerateEffect
-            words="Sonny Taylor"
-            className="relative z-20 text-3xl font-bold text-center text-white md:text-7xl lg:text-6xl"
-        />
-        <Navbar/>
-
-        <Footer/>
-    </div>
-  );
+    );
 }
